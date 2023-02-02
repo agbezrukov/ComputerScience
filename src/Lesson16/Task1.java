79e4b5569e84cc6955366c294a83411f30538226
Kova <a.g.bezrukov@gmail.com>
2023-02-02 10:32:23 +0600

    commit

---
+++ src/Lesson16/Task1.java
@@ -0,0 +1,20 @@
+package src.Lesson16;
+
+/**
+ * Дан порядковый номер месяца. Вывести на консоль пору года, к которой относится месяц
+ */
+public class Task1 {
+
+    public static void main(String[] args) {
+
+        int month = 9;
+
+        switch (month){
+            case 12,1,2 -> System.out.println("Winter");
+            case 3,4,5 -> System.out.println("Spring");
+            case 6,7,8 -> System.out.println("Summer");
+            case 9,10,11 -> System.out.println("Autumn");
+            default -> System.out.println("month is incorrect");
+        }
+    }
+}
---
+++ src/Lesson16/Task2.java
@@ -0,0 +1,17 @@
+package src.Lesson16;
+
+/**
+ * Дано целое число. Определить, является ли последняя цифра этого числа - цифрой 3.
+ */
+public class Task2 {
+    public static void main(String[] args) {
+
+        int number = 123;
+        int remainder10 = number % 10;
+        if (remainder10 == 3){
+            System.out.println("correct");
+        } else{
+            System.out.println("not correct");
+        }
+    }
+}
