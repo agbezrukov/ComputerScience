437ea503df401a67d086636cbceb44ebcbe97630
Kova <a.g.bezrukov@gmail.com>
2023-02-05 00:42:06 +0600

    practice task1-1

--- src/cycles/practicalTask/Task1.java
+++ src/cycles/practicalTask/Task1.java
@@ -7,10 +7,22 @@ public class Task1 {
     // !5 = 1 * 2 * 3 * 4 * 5 = 120
     public static void main(String[] args) {
         int value = 5;
-        System.out.println(factorial(value));
+        System.out.println(factorialFor(value));
+        System.out.println(factorialWhile(value));
     }
 
-    public static int factorial(int value) {
+    public static int factorialWhile(int value){
+    int result = 1;
+    int currentValue = 1;
+
+    while (currentValue <= value){
+        result *= currentValue;
+        currentValue++;
+    }
+return result;
+    }
+
+    public static int factorialFor(int value) {
         int result = 1;
         for (int i = 1; i <= value; i++) {
             result *= i;
