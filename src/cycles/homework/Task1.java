aa8ae07dce46ccc405ecaec1d3ce4978ef8f289e
Kova <a.g.bezrukov@gmail.com>
2023-02-10 21:35:50 +0600

    arrays.lessons

--- src/cycles/homework/Task1.java
+++ src/cycles/homework/Task1.java
@@ -9,17 +9,13 @@ package src.cycles.homework;
  */
 public class Task1 {
     public static void main(String[] args) {
-
         int value = 3345708;
         System.out.println("четных чисел " + isEvenNumbers(value));
         System.out.println("нечетных чисел " + isOddNumbers(value));
-
     }
 
-    public static int isEvenNumbers(int value) {
-
+    private static int isEvenNumbers(int value) {
         int amount = 0;
-
         for (int i = 0, number = value; i < number; number /= 10) {
             int remainder = number % 10;
             if (remainder % 2 == 0) {
@@ -29,10 +25,8 @@ public class Task1 {
         return amount;
     }
 
-    public static int isOddNumbers(int value) {
-
+    private static int isOddNumbers(int value) {
         int amount = 0;
-
         for (int i = 0, number = value; i < number; number /= 10) {
             int remainder = number % 10;
             if (remainder % 2 != 0) {
